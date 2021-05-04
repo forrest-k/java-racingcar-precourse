c5e8b9e8a26545391268548e2833ec2825f20b5d
forrest-k <sjcm888@gmail.com>
2021-05-04 23:07:56 +0900

    refactoring: 클래스명 & 패키지명 리팩토링

--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -1,32 +1,31 @@
+import domain.Car;
+import domain.Cars;
+import domain.MoveStatus;
+import domain.NumberOfRunGames;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import view.RacingResultPrint;
 import java.util.Comparator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RacingTest {
+public class RacingGameTest {
 
 	@Test
 	@DisplayName("자동차_경주_호출")
 	void 자동차_경주_호출() {
-		// Cars 자동차들, int count
+		// domain.Cars 자동차들, int count
+
 		Cars cars = 경주용_자동차_생성("안녕,하세요,쿠쿠씨,다섯대?111");
-		String count = "4";
+		NumberOfRunGames numberOfRunGames = new NumberOfRunGames("4");
 
-		if (!ValidationUtils.isValidCount(count)) {
-			throw new RuntimeException("유효하지 않은 카운트 값");
-		}
-
-		ValidationUtils.isValidCount(count);
-		자동차_경주_시작(cars, Integer.parseInt(count));
+		자동차_경주_시작(cars, numberOfRunGames);
 	}
 
 	@DisplayName("자동차_경주_시작")
-	void 자동차_경주_시작(Cars cars, int count) {
-		Racing racing = new Racing(cars, count);
-		racing.play();
-		assertThat(racing.getRandomNumber()).isLessThan(10);
-		assertThat(racing.getRandomNumber()).isGreaterThanOrEqualTo(0);
+	void 자동차_경주_시작(Cars cars, NumberOfRunGames count) {
+		RacingGame racingGame = new RacingGame(cars, count);
+		RacingResultPrint.showWinners(racingGame.play());
 	}
 
 	@Test
@@ -43,44 +42,37 @@ public class RacingTest {
 	@Test
 	@DisplayName("자동차별_전진_멈춤_여부")
 	void 자동차별_전진_멈춤_여부() {
-		// Cars 자동차들, int count
-		Cars cars = 경주용_자동차_생성("안녕,하세요,쿠쿠씨,다섯대?1");
-		int count = 4;
-		Racing racing = new Racing(cars, count);
-		assertThat(racing.goOrStop()).isNotEmpty();
-		assertThat(racing.goOrStop()).isNotBlank();
-		//assertThat(racing.goOrStop()).matches(Racing.lose);
-//		assertThat(racing.goOrStop()).contains(Racing.lose);
+		assertThat(MoveStatus.goOrStop()).isGreaterThanOrEqualTo(0);
+		assertThat(MoveStatus.goOrStop()).isLessThan(4);
 	}
 
 	@Test
 	@DisplayName("중간_실행_결과_값_출력")
 	void 중간_실행_결과_값_출력() {
 		Cars cars = 경주용_자동차_생성("안녕,하세요,쿠쿠씨,다섯대?1,juddy,mike");
-		int count = 4;
-		RacingResult.interimCheck(cars);
+		RacingResultPrint.interimCheck(cars);
 	}
 
 	@Test
 	@DisplayName("최종_우승자_판단_메소드")
 	void 최종_우승자_판단_메소드() {
 		Cars cars = 경주용_자동차_생성("안녕,하세요,쿠쿠씨,다섯대?1");
-		int count = 10;
-		Racing racing = new Racing(cars, count);
-		racing.play();
+		NumberOfRunGames numberOfRunGames = new NumberOfRunGames("10");
+		RacingGame racingGame = new RacingGame(cars, numberOfRunGames);
+		racingGame.play();
 
 		System.out.println("--- 시작! ---");
 		//System.out.println();
-		for(Car c : cars.getCars()) {
+		for (Car c : cars.getCars()) {
 			System.out.println("name: " + c.getName() + "/ count: " + c.getRacingStatus());
 		}
 
 		cars.getCars().sort(Comparator.reverseOrder());
 		System.out.println("--- 리버스! ---");
-		for(Car c : cars.getCars()) {
+		for (Car c : cars.getCars()) {
 			System.out.println("name: " + c.getName() + "/ count: " + c.getRacingStatus());
 		}
 
-		RacingResult.showWinners(cars);
+		RacingResultPrint.showWinners(racingGame.play());
 	}
 }
