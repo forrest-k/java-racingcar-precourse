e1b1eb135bb920edd8ab6ba4836f4de1c1952422
forrest-k <sjcm888@gmail.com>
2021-05-04 23:04:16 +0900

    feat: 결과값 UI 노출 전용 클래스

---
+++ src/main/java/view/RacingResultPrint.java
@@ -0,0 +1,29 @@
+package view;
+
+import domain.Car;
+import domain.Cars;
+
+import java.util.List;
+
+public class RacingResultPrint {
+
+	public static void interimCheck(Cars cars) {
+		System.out.println();
+		for(Car car : cars.getCars()) {
+			System.out.println(car.getName() + " : " + getStatusBars(car.getRacingStatus()));
+		}
+	}
+
+	private static String getStatusBars(int count) {
+		StringBuilder statusBar = new StringBuilder();
+		for (int i = 0; i < count; i++) {
+			statusBar.append("-");
+		}
+
+		return statusBar.toString();
+	}
+
+	public static void showWinners(List<String> winners) {
+		System.out.println(String.join(", ", winners));
+	}
+}
